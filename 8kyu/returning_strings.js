bc399fe75f2c2a29d3eba57ef455b57613a46e28
Machika Yamauchi <machika111@gmail.com>
2022-11-20 09:34:48 -0800

    upload returning strings challenge

---
+++ 8kyu/returning_strings.js
@@ -0,0 +1,15 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// My solution
+// Parameter: string, ("", " ")
+// return
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+    // return `${name}`
+}
+
+// greet("John") -> "Hello, John how are you doing today?"
+// greet("Mary") -> "Hello, Mary how are you doing today?"
